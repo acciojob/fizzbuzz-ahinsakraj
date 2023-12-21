0382f68a9da66ffd6270391dc776f59eb3691a2a
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 15:06:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function solve(start,end) {
+	let ans="";
+	for(let i=start;i<=end;i++){
+		if(i%3==0 && i%5==0)ans+="FizzBuzz";
+		else if(i%3==0)ans+="Fizz";
+		else if(i%5==0)ans+="Buzz";
+		else ans+=i;
+		ans+="\n";
+	}
+	return ans;
+}
+
+const start=prompt("Enter first number : ");
+const end=prompt("Enter second number : ");
+alert(solve(start,end));
